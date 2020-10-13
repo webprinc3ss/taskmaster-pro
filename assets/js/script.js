b52a4055eaf3a3b2baa9cab6b4e1894a7c47d3ff
Mystic Kitty <djabranton@gmail.com>
2020-10-12 22:57:16 -0700

    Adding moment.

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,27 +87,30 @@ $(".list-group").on("blur", "textarea", function () {
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function () {
+$(".list-group").on("click", "span", function() {
   // get current text
-  var date = $(this)
-    .text()
-    .trim();
+  var date = $(this).text().trim();
 
   // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+  var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+// enable jquery ui datepicker
+dateInput.datepicker({
+  minDate: 1,
+  onClose: function() {
+    // when calendar is closed, force a "change" event on the `dateInput`
+    $(this).trigger("change");
+  }
+});
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -251,6 +254,9 @@ $("#trash").droppable({
 
 });
 
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
 
 // load tasks for the first time
 loadTasks();
